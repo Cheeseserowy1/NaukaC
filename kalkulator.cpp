9410ce564b216be6440ccfb92dd1e8df37e4a9b4
Cheeseserowy1 <152548137+Cheeseserowy1@users.noreply.github.com>
2023-12-01 16:26:21 +0100

    Kalkulator
    Made by Cheeseserowy1.

---
+++ kalkulator.cpp
@@ -0,0 +1,52 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+void kalkulacja();
+
+int main()
+{
+    cout << "Kalkulator made by Cheeseserowy1" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    kalkulacja();
+}
+
+
+void kalkulacja() {
+    int a;
+    int b;
+    string wybor;
+
+    cout << "Podaj A: ";
+    cin >> a;
+    
+    cout << "Podaj B: ";
+    cin >> b;
+
+    cout << "Podaj dzialanie: ";
+    cin >> wybor;
+
+    if (wybor == "+") {
+        a = a + b;
+    }
+    else if (wybor == "-") {
+        a = a - b;
+    }
+    else if (wybor == "*") {
+        a = a * b;
+    }
+    else if (wybor == "/")
+    {
+        a = a / b;
+    }
+    else
+    {
+        cout << "Nie ma takiego działania w tym programie";
+        return;
+    }
+
+    cout << a;
+        
+}
