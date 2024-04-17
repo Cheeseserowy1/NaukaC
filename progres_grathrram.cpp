fd87938a9a164817181c53019051c7375de67c25
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-04-17 09:29:57 +0200

    Update progres_grathrram.cpp

--- progres_grathrram.cpp
+++ progres_grathrram.cpp
@@ -11,13 +11,13 @@ typedef struct color {
   unsigned int b;
 } color;
 
-void print_progres(progres *pro, color Color, color Color2) {
+void print_progres(progres pro, color Color, color Color2) {
   printf("\x1b[%d;2;%d;%d;%dm", 38, Color.r, Color.g, Color.b);
-  for (unsigned int i = 0; i < pro->progres; i++)
+  for (unsigned int i = 0; i < pro.progres; i++)
     std::cout << "■";
 
   printf("\x1b[%d;2;%d;%d;%dm", 38, Color2.r, Color2.g, Color2.b);
-  for (unsigned int i = pro->progres; i < pro->max; i++)
+  for (unsigned int i = pro.progres; i < pro.max; i++)
     std::cout << "■";
   std::cout << std::endl;
   printf("\x1b[0m");
@@ -28,7 +28,7 @@ int main() {
   pro.progres = 5;
   pro.max = 10;
 
-  print_progres(&pro, (color){255, 0, 0},(color){0, 255, 0});
+  print_progres(pro, (color){255, 0, 0},(color){0, 255, 0});
 
   return 0;
 }
